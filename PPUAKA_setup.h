2bd01337825249afaf4dd58c0999df7d2fb27bac
davidwangmj <wangmingjun1987@hotmail.com>
2017-05-17 17:43:34 +0800

    Create PPUAKA_setup.h

---
+++ PPUAKA_setup.h
@@ -0,0 +1,28 @@
+/*
+ * PPUAKA_setup.h
+ *
+ *  Created on: 2017-4-22
+ *      Author: MJWang
+ */
+
+#ifndef PPUAKA_SETUP_H_
+#define PPUAKA_SETUP_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <glib.h>
+#include <pbc.h>
+#include <pbc_random.h>
+#include <pbc_test.h>
+
+
+#include "PPUAKA_lib.h"
+#include "PPUAKA_common.h"
+#include "PPUAKA_core.h"
+
+
+void ppukak_setup( );
+
+#endif /* PPUAKA_SETUP_H_ */
