ebc5ef85f8d788e96fa4c8ed93ebaa1083633dea
davidwangmj <wangmingjun1987@hotmail.com>
2017-05-17 18:05:53 +0800

    Create PPUAKA_register.h

---
+++ PPUAKA_register.h
@@ -0,0 +1,27 @@
+/*
+ * PPUAKA_register.h
+ *
+ *  Created on: 2017-4-22
+ *      Author: MJWang
+ */
+
+#ifndef PPUAKA_REGISTER_H_
+#define PPUAKA_REGISTER_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <glib.h>
+#include <pbc.h>
+#include <pbc_random.h>
+#include <pbc_test.h>
+
+
+#include "PPUAKA_lib.h"
+#include "PPUAKA_common.h"
+#include "PPUAKA_core.h"
+
+void ppuaka_register(int user_num);
+
+#endif /* PPUAKA_REGISTER_H_ */
